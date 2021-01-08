b70b384ee3bf3f1369550bb14b945212f0dbeb9c
Valkkyri <melissa.deshi@gmail.com>
2021-01-07 22:40:50 -0700

    week 01 setup

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let menu = document.querySelector("ol");
+let links = [
+    {
+        label: "Week 01",
+        url: "week-01/index.html"
+    },
+    {
+        label: "Week 02",
+        url: "week-02/index.html"
+    }
+];
+console.log(links);
+
+for (let i = 0; i < links.length; i++)  {
+    let li = document.createElement("li");
+    let link = document.createElement("a");
+    link.setAttribute("href", links[i].url);
+    link.innerHTML = links[i].label;
+    li.appendChild(link);
+    document.querySelector("ol").appendChild(li);
+}
